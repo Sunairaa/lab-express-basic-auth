ace7ec9fbd5ede7c1ea20cf0d32ce01e25b959bb
Sunairaa <sunairashahzeb@gmail.com>
2022-08-17 08:39:54 +0200

    done

--- models/User.model.js
+++ models/User.model.js
@@ -1,14 +1,21 @@
-const { Schema, model } = require("mongoose");
-
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
+// models/User.model.js
+const { Schema, model } = require('mongoose');
+ 
+const userSchema = new Schema(
+{
   username: {
     type: String,
+    trim: true,
+    required: [true, 'Username is required.'],
     unique: true
   },
-  password: String
+  password: {
+    type: String,
+    required: [true, 'Password is required.']
+  }
+},
+{
+  timestamps: true
 });
-
-const User = model("User", userSchema);
-
-module.exports = User;
+ 
+module.exports = model('User', userSchema);
---
+++ routes/auth.routes.js
@@ -0,0 +1,85 @@
+const { Router } = require('express');
+const router = new Router();
+
+// require bcrypt and salt rounds to initialize
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+
+// require user model
+const User = require('../models/User.model');
+
+// GET route ==> to display the signup form to users
+router.get('/signup', (req, res) => res.render('auth/signup'))
+
+// POST route ==> to process form data
+router.post('/signup', (req, res, next) => {
+    const { username, password } = req.body;
+   
+    // encrypt password
+    bcrypt
+      .genSalt(saltRounds)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hashedPassword => {
+        return User.create({
+          username,
+          password: hashedPassword
+        });
+      })
+      .then(userFromDB => {
+        console.log('Newly created user is: ', userFromDB);
+        req.session.currentUser = userFromDB;
+        res.redirect('/auth/userProfile')
+      })
+      .catch(error => next(error));
+  });
+
+//GET route ==> route for user profile
+router.get('/userProfile', (req, res) => {
+  // res.render('users/user-profile', { userInSession: req.session.currentUser });
+  console.log('profile page', req.session);
+  const { username } = req.session.currentUser;
+    res.render('users/user-profile', {username});
+});
+//////////// L O G I N ///////////
+ 
+// GET route ==> to display the login form to users
+router.get('/login', (req, res) => res.render('auth/login'));
+
+// POST login route ==> to process login form data
+router.post('/login', (req, res, next) => {
+  console.log('SESSION =====> ', req.session);
+  const { username, password } = req.body;
+ 
+  if (username === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, username and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: 'User is not registered. Try with other username.' });
+        return;
+      } else if (bcrypt.compareSync(password, user.password)) {
+        //******* SAVE THE USER IN THE SESSION ********//
+        req.session.currentUser = user;
+        res.render('users/user-profile', user);
+        // res.redirect('/auth/userProfile');
+      } else {
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
+    })
+    .catch(error => next(error));
+});
+
+// POST login route ==> to destory session
+// router.post('/logout', (req, res, next) => {
+//   req.session.destroy(err => {
+//     if (err) next(err);
+//     res.redirect('/');
+//   });
+// });
+
+module.exports = router;
